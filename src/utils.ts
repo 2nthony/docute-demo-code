5f7c54ade03245b2213bf7e31cd4c0751d4f58d8
evillt <ijoec123@gmail.com>
2019-04-01 16:22:56 +0800

    fix: only extract export default data object

--- src/utils.ts
+++ src/utils.ts
@@ -15,7 +15,7 @@ export function parseComponent(text: Text): RegisterOptions {
   const component = stripDemoBlock(text).replace(/```html([\s\S]*)\s*```/, '$1')
 
   const JS_RE = /<(script)>([\s\S]*)<\/\1>/
-  const ESM_SCRIPT_RE = /<(script)>\s*export\s*default\s*{\s*([\s\S]*)}\s*<\/\1>/
+  const ESM_SCRIPT_RE = /<(script)>[\s\S]*export\s*default\s*{\s*([\s\S]*)}\s*<\/\1>/
   const [matchedScript = ''] = component.match(JS_RE) || []
   const template = component.replace(JS_RE, '')
   const script = matchedScript.replace(ESM_SCRIPT_RE, '$2')
