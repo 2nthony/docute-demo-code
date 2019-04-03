8be95a934c4e7db6f9799d890bda8014ddf9addc
evillt <ijoec123@gmail.com>
2019-04-03 15:57:27 +0800

    fix: when nothing inside the script tag

--- src/utils.ts
+++ src/utils.ts
@@ -12,10 +12,13 @@ export function stripDemoBlock(text: Text): Text {
 }
 
 export function parseComponent(text: Text): RegisterOptions {
-  const component = stripDemoBlock(text).replace(/```html([\s\S]*)\s*```/, '$1')
+  const component = stripDemoBlock(text).replace(
+    /```(html|vue)([\s\S]*)\s*```/,
+    '$2'
+  )
 
   const JS_RE = /<(script)>([\s\S]*)<\/\1>/
-  const ESM_SCRIPT_RE = /<(script)>[\s\S]*export\s*default\s*{\s*([\s\S]*)}\s*<\/\1>/
+  const MODULE_JS_RE = /<(script)>[\s\S]*export\s*default\s*{\s*([\s\S]*)}\s*<\/\1>/
 
   const [matchedScript = ''] = component.match(JS_RE) || []
 
@@ -23,7 +26,7 @@ export function parseComponent(text: Text): RegisterOptions {
     .replace(JS_RE, '')
     // Replace all style tag to docute built-in component `<v-style>`
     .replace(/<(\/?)style>/g, '<$1v-style>')
-  const script = matchedScript.replace(ESM_SCRIPT_RE, '$2')
+  const script = matchedScript.replace(MODULE_JS_RE, '$2').replace(JS_RE, '')
 
   return {
     template,
