46bca118eb77c9cc4772230a32c41311221697a6
evillt <ijoec123@gmail.com>
2019-04-04 11:14:06 +0800

    chore: format in cli options

--- bili.config.ts
+++ bili.config.ts
@@ -4,8 +4,7 @@ const config: Config = {
   input: 'src/index.ts',
   output: {
     moduleName: 'docuteDemoCode',
-    fileName: 'index[min].js',
-    format: ['umd-min', 'umd']
+    fileName: 'index[min].js'
   }
 }
 
